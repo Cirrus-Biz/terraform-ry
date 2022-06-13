e4ff3712f562c6f81845fda620a84a186f0455e5
noodleween <happyguy122x@gmail.com>
2022-06-13 12:09:16 -0600

    CB-95 Add: vpc, s3 modules

--- main.tf
+++ main.tf
@@ -19,48 +19,21 @@ provider "aws" {
 resource "aws_instance" "app_server" {
   ami           = "ami-0022f774911c1d690"
   instance_type = var.aws_instance_type
+  security_groups = [ "${module.vpc.security_group_private}" ]
+  subnet_id = module.vpc.subnet_id_private
 
   tags = {
-    Name = "aline-ec2-instance"
+    Name = "aline-ec2-RY"
   }
 }
 
-# VPC
-# # EC2 instances
-# module "ec2" {
-#   source = "./modules/ec2"
 
-#   instance_name = "aline-ec2-instance"
-
-#   ami                    = "ami-0022f774911c1d690"
-#   instance_type          = "t2.micro"
-#   key_name               = "JR.Yabut"
-#   monitoring             = true
-#   vpc_security_group_ids = ["sg-12345678"]
-#   subnet_id              = 
-
-#   tags = {
-#     Terraform   = "true"
-#     Environment = "dev"
-#   }
-# }
-
-
-# VPC
-# Subnets (private, public)
+# VPC, Subnets (private, public), route tables, IGW Internet Gateway, NAT gateways
 module "vpc" {
   source = "./modules/vpc"
 }
 
-
-
 # Security Groups
-
-# IGW Internet Gateway
-
-# NAT gateways
-
-# route tables
 
 # IAM roles and policies
 
@@ -82,4 +55,30 @@ module "s3-bucket" {
 
 # Application Load Balancers 
 
-# etc
+### DONE ###
+
+
+
+
+
+
+
+
+# EC2 instances
+# module "ec2" {
+#   source = "./modules/ec2"
+
+#   instance_name = "aline-public-instance-RY"
+
+#   ami                    = "ami-0022f774911c1d690"
+#   instance_type          = "t2.micro"
+#   key_name               = "JR.Yabut"
+#   monitoring             = true
+#   vpc_security_group_ids = [module.security_group_public]
+#   subnet_id              = module.subnet_id_public
+
+#   tags = {
+#     Terraform   = "true"
+#     Environment = "dev"
+#   }
+# }
