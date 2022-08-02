395acab6ef1e07c89d3f318b65f44d22dfb075ad
noodleween <happyguy122x@gmail.com>
2022-08-02 14:52:03 -0600

    CB-79 Refactor: tflint stage

--- main.tf
+++ main.tf
@@ -9,10 +9,10 @@ module "security" {
   vpc_id = module.vpc.vpc_id.id
 }
 
-resource "aws_instance" "foo" {
-  ami           = "ami-0ff8a91507f77f867"
-  instance_type = "t1.2xlarge" # invalid type!
-}
+# resource "aws_instance" "foo" {
+#   ami           = "ami-0ff8a91507f77f867"
+#   instance_type = "t1.2xlarge" # invalid type!
+# }
 # S3 buckets
 
 # Application Load Balancers 
